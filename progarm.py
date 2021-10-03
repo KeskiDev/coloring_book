94bcf6061d96ddef494be0c6f7dd42918fb06512
Lyle olsen <lyleolsen@pop-os.localdomain>
2021-10-02 22:24:28 -0600

    finished getting edge detection working

--- progarm.py
+++ progarm.py
@@ -2,38 +2,34 @@
 import os
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
-img = cv2.imread("/home/lyleolsen/Pictures/parking_lot.jpg", cv2.IMREAD_COLOR)
-grayIMage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def edgeDetection(img, gray, coloring_page):
+    edges = cv2.Canny(gray,threshold1=50, threshold2=300)
 
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(grayIMage,(kernel_size, kernel_size),0)
+    cv2.imshow("original",img)
+    #cv2.imshow("gray image", gray)
+    cv2.imshow("edges", edges)
 
-low_threshold = 50
-high_threshold = 150
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+    reversed = cv2.bitwise_not(edges)
+    cv2.imshow("reversed", reversed)
 
-rho = 1
-theta = np.pi/180
-cv2.threshold = 15
-min_line_length = 50
-max_line_gap = 20
-line_image = np.copy(img) *0
+    file_name = coloring_page + ".jpg"
 
-lines = cv2.HoughLinesP(edges,rho,theta,cv2.threshold,np.array([]),min_line_length,max_line_gap)
+    #status = cv2.imwrite('/home/lyleolsen/Pictures/coloring_pages/' + file_name, reversed)
 
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+    #print("Image written to file-system : ",status)
+    cv2.waitKey(0)
+    
+    cv2.destroyAllWindows()
 
+def process():
+    #file to get
+    img = cv2.imread("/home/lyleolsen/Pictures/xmen.jpg", cv2.IMREAD_COLOR)
+    grayIMage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-line_edges = cv2.addWeighted(img, 0.8, line_image, 1,0)
+    #name of the file in the coloring_page folder
+    coloringPage = "xmen"
+    edgeDetection(img, grayIMage, coloringPage)
 
-cv2.imshow("original", img)
-cv2.imshow("hls image", grayIMage)
-cv2.imshow("lines", line_edges)
-
-
-cv2.waitKey(0)
- 
-cv2.destroyAllWindows()
+process()
